5a4a7988c31a41935a5d8e2e14600ce15e796c20
zhisheng <matrixzheng01@gmail.com>
2023-01-14 22:38:58 +0800

    merge models, predict test labels

---
+++ shell-scripts/main.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## data
+data_cais="None"
+data_ecdt="None"
+data_augment="train.json train_augment.json" # not aug or aug
+
+## pre-training models
+pre_training_models="hfl/chinese-macbert-base"
+
+## model architectures
+model_architectures="0 1 2" # 0-2
+# 0 --> SLUTaggingBERT
+# 1 --> SLUTaggingBERTCascaded
+# 2 --> SLUTaggingBERTMultiHead
+# -1 == > BiLSTM 
+
+## hyperparameters
+lrs=`seq 1e-5 2e-5 5e-5`
+
+bsz=160
+max_epoch=100
+dropout=0.2
+
+use_asr="True False"
+train_mix="True False"
+
+device=4
+
+
+for model in $model_architectures; do
+    for pre_train in $pre_training_models; do
+        for use_aug in $data_augment; do
+            for asr in $use_asr; do
+                for mix in $train_mix; do
+                    for lr in $lrs; do
+                        python scripts/slu_bert.py --train_path $use_aug \
+                            --train_path_cais $data_cais --train_path_ecdt $data_ecdt \
+                            --device $device --lr $lr --max_epoch $max_epoch \
+                            --batch_size $bsz --dropout $dropout --architecture $model \
+                            --model_name $pre_train --use_asr $asr --train_mix $mix
+                    done
+                done
+            done
+        done
+    done
+done
+
--- utils/example_bert.py
+++ utils/example_bert.py
@@ -5,6 +5,7 @@ from utils.vocab import Vocab, LabelVocab
 from utils.word2vec import Word2vecUtils
 from utils.evaluator import Evaluator
 from transformers import BertTokenizer, BertModel
+# from translate import Translator # translate Eng to CHinese
 
 class Example():
 
@@ -15,52 +16,43 @@ class Example():
         cls.word2vec = Word2vecUtils(word2vec_path) # 词向量
         cls.label_vocab = LabelVocab(root,extend_cais=extend_cais,extend_ecdt=extend_ecdt) # ['B', 'I', 'O', '<pad>']
         cls.tokenizer = BertTokenizer.from_pretrained(tokenizer_name)
-        cls.asr=asr
 
     @classmethod
-    def load_dataset(cls, data_path,data_path2=None,data_path3=None):
+    def load_dataset(cls, data_path, extra_data1=None, extra_data2=None, use_asr=False, testing=False):
         datas = json.load(open(data_path, 'r'))
-        examples = []
-        for data in datas:
-            for utt in data:
-                ex = cls(utt)
-                examples.append(ex)
-        if data_path2 is not None:
-            datas = json.load(open(data_path2, 'r'))
-            for data in datas:
-                for utt in data:
-                    ex = cls(utt)
-                    examples.append(ex)
-        if data_path3 is not None:
-            datas = json.load(open(data_path3, 'r'))
-            for data in datas:
-                for utt in data:
-                    ex = cls(utt)
-                    examples.append(ex)
+
+        examples = [cls(utt, use_asr, testing) for data in datas for utt in data]
+
+        if extra_data1 is not None:
+            datas = json.load(open(extra_data1, 'r'))
+            examples.append([cls(utt, use_asr) for data in datas for utt in data])
+
+        if extra_data2 is not None:
+            datas = json.load(open(extra_data2, 'r'))
+            examples.append([cls(utt, use_asr) for data in datas for utt in data])
         return examples
-    
 
-
-    def __init__(self, ex: dict):
+    def __init__(self, ex: dict, use_asr=False, testing=False):
         super(Example, self).__init__()
         self.ex = ex
 
-
-        if self.asr is True:
+        if use_asr:
             self.utt = ex['asr_1best'].upper()
         else:
             self.utt = ex['manual_transcript']
-        
+
         self.slot = {}
-        for label in ex['semantic']:
-            act_slot = f'{label[0]}-{label[1]}'
-            if len(label) == 3:
-                self.slot[act_slot] = label[2]
+        if not testing:
+            for label in ex['semantic']:
+                act_slot = f'{label[0]}-{label[1]}'
+                if len(label) == 3:
+                    self.slot[act_slot] = label[2]
         
         self.tags = ['O'] * len(self.utt)
         self.sep_tag_id=[1] * len(self.utt)
         self.acts = [''] * len(self.utt)
         self.slots = [''] * len(self.utt)
+
         for slot in self.slot:
             value = self.slot[slot]
             bidx = self.utt.find(value)
@@ -78,8 +70,13 @@ class Example():
 
         self.slotvalue = [f'{slot}-{value}' for slot, value in self.slot.items()]
         
+        l = Example.label_vocab
+        self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]
+        self.act_id = [l.convert_act_to_idx(tag) for tag in self.acts]
+        self.slot_id = [l.convert_slot_to_idx(tag) for tag in self.slots]
+
         # self.input_idx_ori = [Example.word_vocab[c] for c in self.utt]
-        self.utt_ori=self.utt
+
 
         ### v1 ###  #78.324
 
@@ -130,8 +127,8 @@ class Example():
         
 
         ### v3 ### 78.44
-        
-        self.utt=self.utt.replace(" ","_").replace("～","~")
+        self.utt_ori = self.utt
+        self.utt = self.utt.replace(" ","_").replace("～","~")
         self.input_idx = Example.tokenizer(self.utt)["input_ids"][1:-1]
 
         # 对英文分词的处理 （逐字符与逐单词不符）
@@ -144,7 +141,11 @@ class Example():
                 word_token = word_token + word_token_ori #c重复当前token，补齐至与word一样长
             if len(word_token) > len(word):
                 word_token=word_token[0:len(word)]
-            i=0
+
+            # bidx = self.utt.find(word)
+            # eidx = bidx + len(word)
+            # self.input_idx = self.input_idx[]
+            i = 0
             while i < (len(self.input_idx) - len(word_token_ori) + 1):
                 if self.input_idx[i:i+len(word_token_ori)] == word_token_ori:
                     self.input_idx[i:i+len(word_token_ori)] = word_token
@@ -152,14 +153,8 @@ class Example():
                 i+=1
 
 
-        ### v3 ###
+        # ### v3 ###
 
-
-
-        l = Example.label_vocab
-        self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]
-        self.act_id = [l.convert_act_to_idx(tag) for tag in self.acts]
-        self.slot_id = [l.convert_slot_to_idx(tag) for tag in self.slots]
-        assert len(self.utt_ori) == len(self.input_idx), f"Mismatch in length: {self.utt_ori} {self.input_idx}"
-        if self.utt_ori!=self.utt:
-            print(self.utt_ori,self.utt)
+        # assert len(self.utt_ori) == len(self.input_idx), f"Mismatch in length: {self.utt_ori} {self.input_idx}"
+        # if self.utt_ori!=self.utt:
+        #     print(self.utt_ori,self.utt)
